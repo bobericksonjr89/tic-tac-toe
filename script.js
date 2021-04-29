8a05019fd3784ab3e3e564d4aac8ee8398403579
Robert Erickson <bobericksonjr@gmail.com>
2021-04-29 14:29:11 -0400

    adds styling and some module functionality

--- script.js
+++ script.js
@@ -1,15 +1,30 @@
 // module
 const gameBoard = (function() {
     const board = ['X', 'O', 'X', 'O', 'X', 'O', 'X', 'O', 'X'];
-
-    function displayBoard(board) {
-        
-    }
+    return {board};
 })();
 
 // module
 const displayController = (function() {
 
+    //DOM
+    const spaceArray = document.querySelectorAll('.board-space')
+
+    function renderBoard(board) {
+        let index = 0;
+        board.forEach(space => populateBoard(space, index++));
+    }
+
+    function populateBoard(space, index) {
+        let para = document.createElement('p');
+        para.innerText = space;
+        para.className = 'game-marker';
+
+        spaceArray[index].appendChild(para);
+        console.log(spaceArray[index]);
+    }
+
+    renderBoard(gameBoard.board);
 })();
 
 // factory
