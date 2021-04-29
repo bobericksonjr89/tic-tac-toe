51b66ef7a8bb6f29b1bd66e43569cf505dc2f586
Robert Erickson <bobericksonjr@gmail.com>
2021-04-29 13:55:40 -0400

    adds basic html and styling

---
+++ script.js
@@ -0,0 +1,18 @@
+// module
+const gameBoard = (function() {
+    const board = ['X', 'O', 'X', 'O', 'X', 'O', 'X', 'O', 'X'];
+
+    function displayBoard(board) {
+        
+    }
+})();
+
+// module
+const displayController = (function() {
+
+})();
+
+// factory
+const Person = function(name) {
+
+};
