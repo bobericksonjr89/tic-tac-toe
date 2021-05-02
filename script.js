6aeb50ea2e7e63be00a7691fa9edd2fdec20cbf0
Robert Erickson <bobericksonjr@gmail.com>
2021-05-02 10:19:02 -0400

    refactors modules and factory

--- script.js
+++ script.js
@@ -1,63 +1,64 @@
-// factory
+// factory - what properties players have and what they can do
 const Person = function(name, icon) {
     const getName = () => name;
     const getIcon = () => icon;
 
-    return {getName, getIcon};
+    const markSpace = (e, spaceIndex) => {
+        gameBoard.boardArray[spaceIndex] = getIcon();   
+    };
+
+    return {getName, markSpace};
 };
 
-// module
-const gameBoard = (function() {
-    const board = ['', '', '', '', '', '', '', '', ''];
-    return {board};
-})();
-
-// module
+// module - controls the flow
 const displayController = (function() {
 
     //DOM
     const boardSpaces = document.querySelectorAll('.board-space')
 
     //Events
-    boardSpaces.forEach(space=> space.addEventListener('click', markSpace));
+    boardSpaces.forEach(space=> space.addEventListener('click', whoseTurn));
 
     let turn = 0;
 
     const player1 = Person("Player 1", "X");
     const player2 = Person("Player 2", "O");
 
-    //functions
-    function markSpace(e) {
+    function whoseTurn(e) {
         const spaceIndex = e.target.dataset.index;
-        if (gameBoard.board[spaceIndex] === '') {
-            const icon = whoseTurn();
-            gameBoard.board[spaceIndex] = icon;
-            renderBoard(gameBoard.board);     
+        if (gameBoard.boardArray[spaceIndex] === '') {
+            turn++;
+            if (turn % 2 !== 0) {
+                player1.markSpace(e, spaceIndex);
+            } else {
+                player2.markSpace(e, spaceIndex);
+            }
+            gameBoard.renderBoard();
             detectWin();
-        }       
+        };
     }
 
-    function whoseTurn() {
-        turn++;
-        if (turn % 2 !== 0) {
-            return player1.getIcon();
-        } else {
-            return player2.getIcon();
-        }
-    }
-
-    function detectWin() {
-
+    function detectWin() {  // MAGIC SQUARE ??
+        return;
     };
+
+    return {boardSpaces};
+})();
+
+// module - board rendering and updating
+const gameBoard = (function() {
+    const boardArray = ['', '', '', '', '', '', '', '', ''];
 
     function renderBoard() {
         let index = 0;
-        gameBoard.board.forEach(marker => populateBoard(marker, index++));
+        boardArray.forEach(marker => populateBoard(marker, index++));
     }
 
     function populateBoard(marker, index) {
-        boardSpaces[index].innerText = marker;
+        displayController.boardSpaces[index].innerText = marker;
     }
 
     renderBoard();
+
+    return {boardArray, renderBoard};
 })();
