3eda204d4bb84a62bb3df0747ef01507f2bb1128
Robert Erickson <bobericksonjr@gmail.com>
2021-05-02 13:56:58 -0400

    adds win conditions and ends game

--- script.js
+++ script.js
@@ -3,8 +3,13 @@ const Person = function(name, icon) {
     const getName = () => name;
     const getIcon = () => icon;
 
-    const markSpace = (e, spaceIndex) => {
+    const markSpace = (spaceIndex) => {
         gameBoard.boardArray[spaceIndex] = getIcon();   
+        if (getIcon() == 'X') {
+            gameBoard.scoreArray[spaceIndex] = 1;
+            return;
+        } // else
+        gameBoard.scoreArray[spaceIndex] = -1;
     };
 
     return {getName, markSpace};
@@ -29,18 +34,82 @@ const displayController = (function() {
         if (gameBoard.boardArray[spaceIndex] === '') {
             turn++;
             if (turn % 2 !== 0) {
-                player1.markSpace(e, spaceIndex);
+                player1.markSpace(spaceIndex);
+                checkTurns(turn, player1)
             } else {
-                player2.markSpace(e, spaceIndex);
+                player2.markSpace(spaceIndex);
+                checkTurns(turn, player2);
             }
             gameBoard.renderBoard();
-            detectWin();
         };
     }
 
+    function checkTurns(turn, player) {
+        if (turn >= 5) {
+            if (detectWin()) {
+                console.log(`${player.getName()} wins!`)
+                endGame();
+            } else if (turn == 9) {
+                console.log("It's a tie!")
+            }
+        }
+    }
+
     function detectWin() {  // MAGIC SQUARE ??
-        return;
+        const scoreArray = gameBoard.scoreArray;
+
+        // row 1
+        let score = scoreArray[0] + scoreArray[1] + scoreArray[2];
+        if (score === 3 || score === -3) {
+            return true;
+        } 
+        // row 2
+        score = scoreArray[3] + scoreArray[4] + scoreArray[5];
+        if (score === 3 || score === -3) {
+            return true;
+        } 
+
+        // row 2
+        score = scoreArray[6] + scoreArray[7] + scoreArray[8];
+        if (score === 3 || score === -3) {
+            return true;
+        } 
+
+        // column 1
+        score = scoreArray[0] + scoreArray[3] + scoreArray[6];
+        if (score === 3 || score === -3) {
+            return true;
+        } 
+
+        // column 2
+        score = scoreArray[1] + scoreArray[4] + scoreArray[7];
+        if (score === 3 || score === -3) {
+            return true;
+        } 
+
+        // column 3
+        score = scoreArray[2] + scoreArray[5] + scoreArray[8];
+        if (score === 3 || score === -3) {
+            return true;
+        } 
+        
+        // diag 1
+        score = scoreArray[0] + scoreArray[4] + scoreArray[8];
+        if (score === 3 || score === -3) {
+            return true;
+        } 
+
+        // diag 2
+        score = scoreArray[2] + scoreArray[4] + scoreArray[6];
+        if (score === 3 || score === -3) {
+            return true;
+        } 
+        return false;
     };
+
+    function endGame() {
+        boardSpaces.forEach(space=> space.removeEventListener('click', whoseTurn));
+    }
 
     return {boardSpaces};
 })();
@@ -48,6 +117,7 @@ const displayController = (function() {
 // module - board rendering and updating
 const gameBoard = (function() {
     const boardArray = ['', '', '', '', '', '', '', '', ''];
+    const scoreArray = [];
 
     function renderBoard() {
         let index = 0;
@@ -60,5 +130,5 @@ const gameBoard = (function() {
 
     renderBoard();
 
-    return {boardArray, renderBoard};
+    return {boardArray, scoreArray, renderBoard};
 })();
